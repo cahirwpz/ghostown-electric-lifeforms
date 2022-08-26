0317fb8d9883da4accf11410487d00e496120987
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-08-26 15:06:03 +0200

    Intro beginning with palette transitions.

--- intro/logo.c
+++ intro/logo.c
@@ -1,26 +1,47 @@
 #include <effect.h>
 #include <blitter.h>
 #include <copper.h>
-#include <fx.h>
-#include <pixmap.h>
-#include <sprite.h>
+#include <color.h>
+#include <sync.h>
 #include <system/memory.h>
 
 #define WIDTH 320
 #define HEIGHT 256
 #define DEPTH 3
 
-#include "data/electric-lifeforms.c"
+#include "data/ghostown-logo-01.c"
+#include "data/ghostown-logo-02.c"
+#include "data/ghostown-logo-03.c"
+#include "data/ghostown-logo-crop.c"
 
 static CopListT *cp;
+static BitmapT *screen;
+
+static const PaletteT *ghostown_logo_pal[] = {
+  NULL,
+  &ghostown_logo_1_pal,
+  &ghostown_logo_2_pal,
+  &ghostown_logo_3_pal,
+};
+
+extern TrackT GhostownLogoPal;
 
 static void Init(void) {
+  screen = NewBitmap(WIDTH, HEIGHT, DEPTH);
+
   SetupPlayfield(MODE_LORES, DEPTH, X(0), Y(0), WIDTH, HEIGHT);
-  LoadPalette(&logo_pal, 0);
+
+  EnableDMA(DMAF_BLITTER);
+  BitmapCopy(screen, (WIDTH - ghostown_logo_width) / 2,
+             (HEIGHT - ghostown_logo_height) / 2, &ghostown_logo);
+  WaitBlitter();
+  DisableDMA(DMAF_BLITTER);
+
+  TrackInit(&GhostownLogoPal);
 
   cp = NewCopList(40);
   CopInit(cp);
-  CopSetupBitplanes(cp, NULL, &logo, DEPTH);
+  CopSetupBitplanes(cp, NULL, screen, DEPTH);
   CopEnd(cp);
 
   CopListActivate(cp);
@@ -32,9 +53,26 @@ static void Kill(void) {
   DisableDMA(DMAF_COPPER | DMAF_RASTER);
 
   DeleteCopList(cp);
+
+  DeleteBitmap(screen);
 }
 
 static void Render(void) {
+  short num = TrackValueGet(&GhostownLogoPal, frameCount);
+  short fromCurrKeyFrame = frameCount - CurrKeyFrame(&GhostownLogoPal);
+
+  if (fromCurrKeyFrame < 16) {
+    short i;
+
+    for (i = 0; i < (1 << DEPTH); i++) {
+      short prev = (num == 1) ? 0 : ghostown_logo_pal[num - 1]->colors[i];
+      short curr = ghostown_logo_pal[num]->colors[i];
+      SetColor(i, ColorTransition(prev, curr, fromCurrKeyFrame));
+    }
+  } else {
+    LoadPalette(ghostown_logo_pal[num], 0);
+  }
+
   TaskWaitVBlank();
 }
 
