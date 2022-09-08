4b919199c1e1d1e2fa37f360dfef9b507c1b7140
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2022-09-08 15:49:17 +0200

    Put all necessary data into rawpng binary.

--- intro/pngraw.c
+++ intro/pngraw.c
@@ -6,30 +6,13 @@
 #define PNG_GRAYSCALE_ALPHA 4
 #define PNG_TRUECOLOR_ALPHA 6
 
-typedef struct {
-  int width;
-  int height;
-  char bit_depth;
-  char colour_type;
-  char compression_method;
-  char filter_method;
-  char interlace_method;
-} IHDR;
-
-#define BYTES(x) (((x) + 7) >> 3)
-
-static __regargs short GetBitsPerPixel(IHDR *ihdr) {
-  short bpp = ihdr->bit_depth;
-
-  if (ihdr->colour_type == PNG_GRAYSCALE_ALPHA)
-    bpp *= 2;
-  else if (ihdr->colour_type == PNG_TRUECOLOR)
-    bpp *= 3;
-  else if (ihdr->colour_type == PNG_TRUECOLOR_ALPHA)
-    bpp *= 4;
-
-  return bpp;
-}
+typedef struct RawPng {
+  short width;
+  short height;
+  short row_size;
+  short pix_size;
+  u_char data[0];
+} RawPngT;
 
 static inline short PaethPredictor(short a, short b, short c) {
   short p = a + b - c;
@@ -52,21 +35,16 @@ static inline short PaethPredictor(short a, short b, short c) {
   return c;
 }
 
-void ReconstructImage(u_char *pixels, u_char *encoded, IHDR *ihdr) {
-  short bpp = GetBitsPerPixel(ihdr);
-  short row = BYTES(bpp * (short)ihdr->width);
-  short pixel = BYTES(bpp);
-  short n = ihdr->height;
-  short i, j;
+#define BYTES(x) (((x) + 7) >> 3)
 
-  for (i = 0; i < n; i++) {
+void RawPngDecode(RawPngT *rawpng) {
+  u_char *pixels = rawpng->data;
+  u_char *encoded = rawpng->data;
+  register short n asm("d6") = rawpng->height - 1;
+  register short j asm("d7");
+
+  do {
     u_char method = *encoded++;
-
-    if (method == 2 && i == 0)
-      method = 0;
-
-    if (method == 4 && i == 0)
-      method = 1;
 
     /*
      * Filters are applied to corresponding (!) bytes, not to pixels, regardless
@@ -75,60 +53,49 @@ void ReconstructImage(u_char *pixels, u_char *encoded, IHDR *ihdr) {
      */
 
     if (method == 0) {
-      j = row - 1;
+      j = rawpng->row_size - 1;
       do
         *pixels++ = *encoded++;
       while (--j != -1);
     } else if (method == 1) {
       u_char *left = pixels;
-      j = pixel - 1;
+      j = rawpng->pix_size - 1;
       do
         *pixels++ = *encoded++;
       while (--j != -1);
-      j = row - pixel - 1;
+      j = rawpng->row_size - rawpng->pix_size - 1;
       do
         *pixels++ = *encoded++ + *left++;
       while (--j != -1);
     } else if (method == 2) {
-      u_char *up = pixels - row;
-      j = row - 1;
+      u_char *up = pixels - rawpng->row_size;
+      j = rawpng->row_size - 1;
       do
         *pixels++ = *encoded++ + *up++;
       while (--j != -1);
     } else if (method == 3) {
       u_char *left = pixels;
-      if (i > 0) {
-        u_char *up = pixels - row;
-        j = pixel - 1;
-        do
-          *pixels++ = *encoded++ + *up++ / 2;
-        while (--j != -1);
-        j = row - pixel - 1;
-        do
-          *pixels++ = *encoded++ + (*left++ + *up++) / 2;
-        while (--j != -1);
-      } else {
-        j = pixel - 1;
-        do
-          *pixels++ = *encoded++;
-        while (--j != -1);
-        j = row - pixel - 1;
-        do
-          *pixels++ = *encoded++ + *left++ / 2;
-        while (--j != -1);
-      }
+      u_char *up = pixels - rawpng->row_size;
+      j = rawpng->pix_size - 1;
+      do
+        *pixels++ = *encoded++ + ((short)*up++ >> 1);
+      while (--j != -1);
+      j = rawpng->row_size - rawpng->pix_size - 1;
+      do
+        *pixels++ = *encoded++ + ((short)(*left++ + *up++) >> 1);
+      while (--j != -1);
     } else if (method == 4) {
       u_char *left = pixels;
-      u_char *leftup = pixels - row;
-      u_char *up = pixels - row;
-      j = pixel - 1;
+      u_char *leftup = pixels - rawpng->row_size;
+      u_char *up = pixels - rawpng->row_size;
+      j = rawpng->pix_size - 1;
       do
         *pixels++ = *encoded++ + *up++;
       while (--j != -1);
-      j = row - pixel - 1;
+      j = rawpng->row_size - rawpng->pix_size - 1;
       do {
         *pixels++ = *encoded++ + PaethPredictor(*left++, *up++, *leftup++);
       } while (--j != -1);
     }
-  }
+  } while (--n != -1);
 }
--- intro/pngraw.py
+++ intro/pngraw.py
@@ -2,6 +2,7 @@
 
 from binascii import hexlify
 from collections import namedtuple
+from enum import IntEnum
 
 import argparse
 import struct
@@ -9,23 +10,32 @@ import sys
 import zlib
 
 
-IHDR = namedtuple('IHDR',
-                  'width height bpp color compression filter interlace')
+class ColourType(IntEnum):
+    GRAYSCALE = 0
+    TRUECOLOR = 2
+    INDEXED = 3
+    GRAYSCALE_ALPHA = 4
+    TRUECOLOR_ALPHA = 6
+
+
+IHDR = namedtuple('IHDR', ('width height bit_depth colour_type' +
+                           ' compression filter interlace'))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
-        description='Encode 8-bit samples with IMA ADPCM encoding.')
-    parser.add_argument(
-        'png', metavar='PNG', type=str, help='PNG image optimized with optipng.')
-    parser.add_argument(
-        'raw', metavar='RAW', type=str, help='Raw decoded IHDR & IDAT chunk.')
+        description='Write uncompressed PNG IDAT chunk to file.')
+    parser.add_argument('png', metavar='PNG', type=str,
+                        help='PNG image optimized with optipng.')
+    parser.add_argument('raw', metavar='RAW', type=str,
+                        help='Raw decoded IHDR & IDAT chunk.')
     args = parser.parse_args()
 
     chunks = dict()
 
     with open(args.png, 'rb') as f:
-        print(hexlify(f.read(8)))
+        assert f.read(8) == b'\x89\x50\x4e\x47\x0d\x0a\x1a\x0a'
+
         while hdr := f.read(8):
             size, chunkId = struct.unpack('>I4s', hdr)
             chunkType = chunkId.decode()
@@ -34,12 +44,34 @@ if __name__ == '__main__':
             calc_crc = zlib.crc32(chunkId + chunk)
             if chunkType == 'IDAT':
                 chunk = zlib.decompress(chunk)
+                assert chunk[0] < 2, "method check failed"
             if chunkType == 'IHDR':
-                print(IHDR(*struct.unpack('>IIbbbbb', chunk)))
+                chunk = IHDR(*struct.unpack('>IIbbbbb', chunk))
+                assert chunk.colour_type <= 3, "colour type check failed"
+                assert chunk.compression == 0, "compression check failed"
+                assert chunk.filter == 0, "filter check failed"
+                assert chunk.interlace == 0, "interlace check failed"
+                chunk = chunk._replace(
+                        colour_type=ColourType(chunk.colour_type))
+                print(chunk)
             chunks[chunkType] = chunk
             file_crc = struct.unpack('>I', f.read(4))[0]
             assert calc_crc == file_crc
 
     with open(args.raw, 'wb') as f:
-        f.write(chunks['IHDR'])
+        ihdr = chunks['IHDR']
+        bpp = ihdr.bit_depth
+
+        if ihdr.colour_type == ColourType.GRAYSCALE_ALPHA:
+            bpp *= 2
+        elif ihdr.colour_type == ColourType.TRUECOLOR:
+            bpp *= 3
+        elif ihdr.colour_type == ColourType.TRUECOLOR_ALPHA:
+            bpp *= 4
+
+        row_size = (bpp * width + 7) // 8
+        pix_size = (bpp + 7) // 8
+
+        hdr = struct.pack('>HHHH', ihdr.width, ihdr.height, row_size, pix_size)
+        f.write(hdr)
         f.write(chunks['IDAT'])
