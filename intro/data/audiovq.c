2139054c46bf6a4ede864cec73cf0e296deb7fcd
Krystian Bacławski <krystian.baclawski@gmail.com>
2022-09-11 13:08:45 +0200

    Implement kmeans++ algorithm.

--- intro/data/audiovq.c
+++ intro/data/audiovq.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <math.h>
 #include <float.h>
@@ -13,35 +14,51 @@
 typedef float vec_t __attribute__((vector_size(VECLEN * sizeof(float))));
 
 typedef struct codestat {
+  double dist;
   int count;
-  float dist;
 } codestat_t;
+
+typedef struct nearest {
+  double dist;
+  int idx;
+  bool chosen;
+} nearest_t;
 
 static inline float Euclid(vec_t a, vec_t b) {
   vec_t d = a - b;
   vec_t sq = d * d;
-  return sqrtf(sq[0] + sq[1] + sq[2] + sq[3]);
+  return sq[0] + sq[1] + sq[2] + sq[3];
 }
 
-static inline vec_t DivVector(vec_t vec, long n) {
-  return vec / (vec_t){n, n, n, n};
-}
-
-static vec_t CenterVector(vec_t *vec, long n) {
-  vec_t center = (vec_t){0};
-  for (long i = 0; i < n; i++)
-    center += vec[i];
-  return DivVector(center, n);
-}
-
-static float AverageDistortion(vec_t *codebook, int *nearest,
-                               vec_t *vec, long n) {
+static float AverageDistortion(const nearest_t *nearest, const vec_t *vec,
+                               long n, const vec_t *codebook) {
   float d = 0.0;
 
   for (long i = 0; i < n; i++)
-    d += Euclid(codebook[nearest[i]], vec[i]);
+    d += Euclid(codebook[nearest[i].idx], vec[i]);
 
   return d / n;
+}
+
+// For each vector find its nearest codeword
+static void FindNearest(nearest_t *nearest, const vec_t *vec, long n,
+                        const vec_t *codebook, long codewords) {
+  for (long i = 0; i < n; i++) {
+    float best_dist = FLT_MAX;
+    int best_idx = -1;
+    for (long j = 0; j < codewords; j++) {
+      float dist = Euclid(codebook[j], vec[i]);
+      if (dist < best_dist) {
+        best_dist = dist;
+        best_idx = j;
+      }
+    }
+
+    assert(best_idx >= 0 && best_idx < codewords);
+
+    nearest[i].idx = best_idx;
+    nearest[i].dist = best_dist;
+  }
 }
 
 void DumpCodeBook(vec_t *codebook, codestat_t *codestat, long n) {
@@ -50,87 +67,105 @@ void DumpCodeBook(vec_t *codebook, codestat_t *codestat, long n) {
     codestat_t stat = codestat[i];
     if (!stat.count)
       continue;
-    printf("codebook[%ld] = <%f %f %f %f> (count: %d, dist: %f)\n",
-           i, vec[0], vec[1], vec[2], vec[3], stat.count, stat.dist);
+    printf("codebook[%ld] = <%d %d %d %d> (count: %d, dist: %f)\n", i,
+           (int)vec[0], (int)vec[1], (int)vec[2], (int)vec[3], stat.count,
+           stat.dist);
   }
 }
 
-// Linde-Buzo-Gray / Generalized Lloyd algorithm
-// Loosely based on: https://github.com/internaut/py-lbg/blob/master/lbg.py
-static float EncodeChunk(vec_t *vec, size_t n, double epsilon) {
+// Update codebook based on fresh vectors classification
+static void UpdateCodeBook(vec_t *codebook, codestat_t *codestat,
+                           long codewords, const nearest_t *nearest,
+                           const vec_t *vec, long n) {
+  long i;
+
+  memset(codebook, 0, codewords * sizeof(vec_t));
+  memset(codestat, 0, codewords * sizeof(codestat_t));
+
+  for (i = 0; i < n; i++) {
+    int idx = nearest[i].idx;
+    float dist = nearest[i].dist;
+    codestat[idx].count++;
+    codestat[idx].dist += dist;
+    codebook[idx] += vec[i];
+  }
+
+  for (i = 0; i < codewords; i++) {
+    long cnt = codestat[i].count;
+    if (cnt)
+      codebook[i] /= (vec_t){cnt, cnt, cnt, cnt};
+  }
+}
+
+// K-Means++ algorithm adapted to 8-bit 4-element vectors
+// http://ilpubs.stanford.edu:8090/778/1/2006-13.pdf
+static float EncodeChunk(const vec_t *vec, size_t n, double epsilon) {
   static vec_t codebook[CODEWORDS];
   static codestat_t codestat[CODEWORDS];
-  static int nearest[MAXVECNUM];
+  static nearest_t nearest[MAXVECNUM];
+
+  long i, codewords;
 
   memset(codebook, 0, sizeof(codebook));
   memset(codestat, 0, sizeof(codestat));
   memset(nearest, 0, sizeof(nearest));
 
-  codebook[0] = CenterVector(vec, n);
+  // Take one center `c` chosen uniformly at random from `X`.
+  i = random() % n;
+  nearest[i].chosen = true;
+  codebook[0] = vec[i];
 
-  float avg_dist = AverageDistortion(codebook, nearest, vec, n);
-  int niter;
+  // Take a new center `c_i`, choosing `x` in `X`
+  // with probability D(x)^2 / sum(D(x)^2) for x in X)
+  for (codewords = 1; codewords < CODEWORDS;) {
+    FindNearest(nearest, vec, n, codebook, codewords);
 
-  codestat[0] = (codestat_t){.count = n, .dist = avg_dist};
-
-  for (int codewords = 1; codewords < CODEWORDS;) {
-    int i, j;
-
-    for (int c = 0; c < codewords; c++) {
-      codebook[codewords + c] = codebook[c] + (vec_t){1,1,1,1};
-      codebook[c] -= (vec_t){1,1,1,1};
+    long last = 0;
+    for (i = 0; i < n; i++) {
+      if (nearest[i].chosen)
+        nearest[i].dist = 0;
+      nearest[i].dist += last;
+      last = nearest[i].dist;
     }
 
-    codewords *= 2;
+    if (last == 0)
+      break;
 
-    // Try to reach a convergence by minimizing the average distortion.
-    // This is done by moving the codewords step by step to the center
-    // of the points in their proximity.
-    double err = epsilon + 1;
+    long x = random() % last;
 
-    for (niter = 0; err > epsilon; niter++) {
-      memset(nearest, 0, sizeof(nearest));
-      memset(codestat, 0, sizeof(codestat));
+    for (i = 0; i < n; i++)
+      if (x < nearest[i].dist)
+        break;
 
-      // For each vector find its nearest codeword
-      for (i = 0; i < n; i++) {
-        float min_dist = FLT_MAX;
-        int best_idx = -1;
-        for (j = 0; j < codewords; j++) {
-          float dist = Euclid(codebook[j], vec[i]);
-          if (dist < min_dist) {
-            min_dist = dist;
-            best_idx = j;
-          }
-        }
+    assert(!nearest[i].chosen);
 
-        nearest[i] = best_idx;
-        codestat[best_idx].count++;
-        codestat[best_idx].dist += min_dist;
-      }
-
-      // Update codebook based on fresh vectors classification
-      memset(codebook, 0, codewords * sizeof(vec_t));
-
-      for (i = 0; i < n; i++)
-        codebook[nearest[i]] += vec[i];
-
-      for (i = 0; i < codewords; i++)
-        if (codestat[i].count)
-          codebook[i] = DivVector(codebook[i], codestat[i].count);
-
-      // Recalculate average distortion value & error value
-      float prev_avg_dist = avg_dist;
-
-      avg_dist = AverageDistortion(codebook, nearest, vec, n);
-      // assert(avg_dist <= prev_avg_dist);
-      err = fabs(prev_avg_dist - avg_dist) / prev_avg_dist;
-    }
+    nearest[i].chosen = true;
+    codebook[codewords++] = vec[i];
   }
 
-  DumpCodeBook(codebook, codestat, CODEWORDS);
+  // Try to reach a convergence by minimizing the average distortion.
+  // This is done by moving the codewords step by step to the center
+  // of the points in their proximity.
+  double err = epsilon + 1;
+  float avg_dist = AverageDistortion(nearest, vec, n, codebook);
+  int niter;
 
-  return avg_dist;
+  for (niter = 0; err > epsilon; niter++) {
+    FindNearest(nearest, vec, n, codebook, codewords);
+    UpdateCodeBook(codebook, codestat, codewords, nearest, vec, n);
+
+    // Recalculate average distortion value & error value
+    float prev_avg_dist = avg_dist;
+
+    avg_dist = AverageDistortion(nearest, vec, n, codebook);
+    // printf("[%d] avg_dist = %f\n", niter, avg_dist);
+    assert(avg_dist <= prev_avg_dist);
+    err = fabs(prev_avg_dist - avg_dist) / prev_avg_dist;
+  }
+
+  // DumpCodeBook(codebook, codestat, CODEWORDS);
+
+  return sqrt(avg_dist);
 }
 
 static void EncodeFile(FILE *f) {
@@ -147,13 +182,11 @@ static void EncodeFile(FILE *f) {
 
     size_t n = (len + 3) / 4;
 
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < VECLEN; j++) {
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < VECLEN; j++)
         vec[i][j] = data[i * 4 + j] + 128;
-      }
-    }
 
-    double avg_dist = EncodeChunk(vec, n, 0.0001);
+    double avg_dist = EncodeChunk(vec, n, 0.001);
 
     printf("chunk[%d] avg_dist %g\n", nchunks, avg_dist);
 
