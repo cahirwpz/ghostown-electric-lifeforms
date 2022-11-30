c61a0513f93e406d871a6071ec563244e0f0b6de
Krystian Bacławski <cahirwpz@cs.uni.wroc.pl>
2022-11-30 16:08:34 +0100

    Avoid long multiplications.

--- effects/donut/donut.h
+++ effects/donut/donut.h
@@ -16,35 +16,39 @@ static char b[W * H], z[W * H];
 static const char pixels[] = ".,-~:;=!*#$@";
 
 void CalcDonut(void) {
-  static int sA = 1024, cA = 0, sB = 1024, cB = 0, _;
+  static short sA = 1024, cA = 0, sB = 1024, cB = 0, _;
 
-  int sj = 0, cj = 1024;
-  int i, j;
+  short sj = 0, cj = 1024;
+  short i, j;
 
   memset(b, 32, W*H);  // text buffer
   memset(z, 127, W*H); // z buffer
 
   for (j = 0; j < 90; j++) {
-    int si = 0, ci = 1024; // sine and cosine of angle i
+    // sine and cosine of angle i
+    short si = 0;
+    short ci = 1024;
 #ifdef Log
     Log("j = %d\n", j);
 #endif
     for (i = 0; i < 324; i++) {
-      int R1 = 1, R2 = 2048, K2 = 5120 * 1024;
+      short R1 = 1;
+      short R2 = 2048;
+      int K2 = 5120 * 1024;
 
-      int x0 = R1 * cj + R2;
-      int x1 = ci * x0 >> 10;
-      int x2 = cA * sj >> 10;
-      int x3 = si * x0 >> 10;
-      int x4 = R1 * x2 - (sA * x3 >> 10);
-      int x5 = sA * sj >> 10;
+      /* int */ short x0 = R1 * cj + R2;
+      /* int */ short x1 = ci * x0 >> 10;
+      /* int */ short x2 = cA * sj >> 10;
+      /* int */ short x3 = si * x0 >> 10;
+      /* int */ short x4 = R1 * x2 - (sA * x3 >> 10);
+      /* int */ short x5 = sA * sj >> 10;
       int x6 = K2 + R1 * 1024 * x5 + cA * x3;
-      int x7 = cj * si >> 10;
-      int x = W / 2 + D * (cB * x1 - sB * x4) / x6;
-      int y = H / 2 + D / 2 * (cB * x4 + sB * x1) / x6;
-      int _N1 = cB * ((-sA * x7 >> 10) + x2);
-      int _N2 = ci * (cj * sB >> 10);
-      int N = (((-cA * x7 - _N1 - _N2) >> 10) - x5) >> 7;
+      /* int */ short x7 = cj * si >> 10;
+      /* int */ short x = W / 2 + D * (cB * x1 - sB * x4) / x6;
+      /* int */ short y = H / 2 + D / 2 * (cB * x4 + sB * x1) / x6;
+      short _N1 = (-sA * x7 >> 10) + x2;
+      short _N2 = cj * sB >> 10;
+      int N = (((-cA * x7 - cB * _N1 - ci * _N2) >> 10) - x5) >> 7;
 
       int o = x + W * y;
       char zz = (x6 - K2) >> 15;
@@ -61,4 +65,3 @@ void CalcDonut(void) {
   R(5, 7, cA, sA);
   R(5, 8, cB, sB);
 }
-
