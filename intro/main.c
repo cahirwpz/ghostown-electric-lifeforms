cb2fd23e206ddc6cef82bda38322c9b96587af97
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-02-11 17:01:50 +0100

    Reset module position back to 0

--- intro/main.c
+++ intro/main.c
@@ -127,7 +127,7 @@ static void UnLoadEffects(EffectT **effects) {
 
 static void RunEffects(void) {
   /* Set the beginning of intro. Useful for effect synchronization! */
-  short pos = 0x820;
+  short pos = 0;
 
   frameCount = SYNCPOS(pos);
   SetFrameCounter(frameCount);
