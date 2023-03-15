28d700fbca6d26bb14f8e18112a1d0da5c54084c
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-03-15 22:09:33 +0100

    Separate pixel doubling code.

--- intro/game-of-life.c
+++ intro/game-of-life.c
@@ -121,8 +121,9 @@ static CopInsT *palptr;
 
 static CopInsT *sprptr[8];
 
-#include "gol-games.c"
 #include "gol-cycling.c"
+#include "gol-doubling.c"
+#include "gol-games.c"
 #include "gol-palette.c"
 #include "gol-pingpong.c"
 
@@ -151,23 +152,6 @@ static short next_spawn[128];
 static const ElectronArrayT *cur_electrons;
 
 static const GameDefinitionT *current_game;
-
-// Used by CPU to quickly transform 1x1 pixels into 2x1 pixels.
-static u_short double_pixels[256];
-
-static void MakeDoublePixels(void) {
-  u_short *data = double_pixels;
-  u_short w = 0;
-  short i;
-
-  for (i = 0; i < 256; i++) {
-    *data++ = w;
-    w |= 0xAAAA; /* let carry propagate through odd bits */
-    w++;
-    w &= 0x5555;
-    w |= w << 1;
-  }
-}
 
 // setup blitter to calculate a function of three horizontally adjacent lit
 // pixels the setup in this blit is as follows (what data each channel sees):
@@ -291,40 +275,6 @@ static void WireworldSwitch(__unused const BitmapT *sourceA,
   SpawnElectrons(cur_electrons, wireworld_step ^ 1, wireworld_step);
 }
 
-static void (*PixelDouble)(u_char *source asm("a0"), u_short *target asm("a1"),
-                           u_short *lut asm("a2"));
-
-#define PixelDoubleSize \
-  ((BOARD_WIDTH / 8) * BOARD_HEIGHT * 10 + BOARD_HEIGHT * 2 + 6)
-
-// doubles pixels horizontally
-static void MakePixelDoublingCode(const BitmapT *bitmap) {
-  u_short x;
-  u_short y;
-  u_short *code = (void *)PixelDouble;
-
-  *code++ = 0x7200 | (EXT_BOARD_MODULO & 0xFF); // moveq #EXT_BOARD_MODULO,d1
-  for (y = EXT_HEIGHT_TOP; y < bitmap->height - EXT_HEIGHT_BOTTOM; y++) {
-    for (x = EXT_WIDTH_LEFT / 8; x < bitmap->bytesPerRow - EXT_WIDTH_RIGHT / 8;
-         x++) {
-      *code++ = 0x7000; // moveq.l #0,d0           # 4
-      *code++ = 0x1018; // move.b (a0)+,d0         # 8
-      *code++ = 0xd040; // add.w  d0,d0            # 4
-      *code++ = 0x32f2;
-      *code++ = 0x0000; // move.w (a2,d0.w),(a1)+  # 18
-      // perform a lookup in the pixel doubling lookup table
-      // (e.g. 00100110 -> 0000110000111100)
-      // *double_target++ = double_pixels[*double_src++];
-    }
-    *code++ = 0xD1C1; // adda.l d1,a0
-    // double_src += EXT_BOARD_MODULO & 0xFF;
-    // bitmap modulo - skip the extra EXT_BOARD_MODULO bytes on the edges
-    // (EXT_WIDTH_LEFT/8 bytes on the left, EXT_WIDTH_RIGHT/8 bytes on the right
-    // on the next row)
-  }
-  *code++ = 0x4e75; // rts
-}
-
 static void MakeCopperList(CopListT *cp) {
   u_short i;
 
@@ -428,7 +378,7 @@ static void SharedPreInit(void) {
 
     MakeDoublePixels();
     PixelDouble = MemAlloc(PixelDoubleSize, MEMF_PUBLIC);
-    MakePixelDoublingCode(boards[0]);
+    MakePixelDoublingCode();
 
     pingpong = MemAlloc(sizeof(ColorPingPongT), MEMF_PUBLIC);
     MakeColorPingPong(pingpong);
---
+++ intro/gol-doubling.c
@@ -0,0 +1,47 @@
+// Used by CPU to quickly transform 1x1 pixels into 2x1 pixels.
+static u_short double_pixels[256];
+
+static void MakeDoublePixels(void) {
+  u_short *data = double_pixels;
+  u_short w = 0;
+  short i;
+
+  for (i = 0; i < 256; i++) {
+    *data++ = w;
+    w |= 0xAAAA; /* let carry propagate through odd bits */
+    w++;
+    w &= 0x5555;
+    w |= w << 1;
+  }
+}
+
+static void (*PixelDouble)(u_char *source asm("a0"), u_short *target asm("a1"),
+                           u_short *lut asm("a2"));
+
+#define PixelDoubleSize \
+  ((BOARD_WIDTH / 8) * BOARD_HEIGHT * 10 + BOARD_HEIGHT * 2 + 6)
+
+static void MakePixelDoublingCode(void) {
+  u_short *code = (void *)PixelDouble;
+  u_short x, y;
+
+  *code++ = 0x7200 | (EXT_BOARD_MODULO & 0xFF); // moveq #EXT_BOARD_MODULO,d1
+  for (y = 0; y < BOARD_HEIGHT; y++) {
+    for (x = 0; x < BOARD_WIDTH / 8; x++) {
+      *code++ = 0x7000; // moveq.l #0,d0           # 4
+      *code++ = 0x1018; // move.b (a0)+,d0         # 8
+      *code++ = 0xd040; // add.w  d0,d0            # 4
+      *code++ = 0x32f2;
+      *code++ = 0x0000; // move.w (a2,d0.w),(a1)+  # 18
+      // perform a lookup in the pixel doubling lookup table
+      // (e.g. 00100110 -> 0000110000111100)
+      // *double_target++ = double_pixels[*double_src++];
+    }
+    *code++ = 0xD1C1; // adda.l d1,a0
+    // double_src += EXT_BOARD_MODULO & 0xFF;
+    // bitmap modulo - skip the extra EXT_BOARD_MODULO bytes on the edges
+    // (EXT_WIDTH_LEFT/8 bytes on the left, EXT_WIDTH_RIGHT/8 bytes on the right
+    // on the next row)
+  }
+  *code++ = 0x4e75; // rts
+}
