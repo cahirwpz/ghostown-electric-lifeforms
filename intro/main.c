41c2a1510b286ab1323b4cd38522bd78ae92f456
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-03-18 14:37:33 +0100

    Progress bar for the time AmigaKlang generates instruments (#34)

--- intro/main.c
+++ intro/main.c
@@ -10,9 +10,6 @@
 
 extern u_char Module[];
 extern u_char Samples[];
-#if VQ == 1 || DELTA == 1 || KLANG == 0
-extern u_char SamplesSize[];
-#endif
 
 extern EffectT LogoEffect;
 extern EffectT WeaveEffect;
@@ -45,73 +42,6 @@ static EffectT *AllEffects[] = {
   &UVMapEffect,
   NULL,
 };
-
-#if DELTA == 1
-static void DecodeSamples(u_char *smp, int size) {
-  u_char data = *smp++;
-  short n = (size + 7) / 8 - 1;
-  short k = size & 7;
-
-  Log("[Init] Decoding delta samples (%d bytes)\n", size);
-
-  switch (k) {
-  case 0: do { data += *smp; *smp++ = data;
-  case 7:      data += *smp; *smp++ = data;
-  case 6:      data += *smp; *smp++ = data;
-  case 5:      data += *smp; *smp++ = data;
-  case 4:      data += *smp; *smp++ = data;
-  case 3:      data += *smp; *smp++ = data;
-  case 2:      data += *smp; *smp++ = data;
-  case 1:      data += *smp; *smp++ = data;
-          } while (--n != -1);
-  }
-}
-#endif
-
-#if VQ == 1
-static void DecodeSamples(u_char *smp, int size) {
-  char *copy = MemAlloc(size, MEMF_PUBLIC);
-  memcpy(copy, smp, size);
-
-  Log("[Init] Decoding VQ samples (%d bytes)\n", size);
-  
-  {
-    u_char *data = copy;
-    u_char *out = smp;
-
-    for (;;) {
-      u_char *codebook;
-      short n;
-      
-      n = (*data++) << 8;
-      n |= *data++;
-      
-      if (n == 0)
-        break;
-
-      codebook = data;
-      data += 1024;
-      n--;
-
-      do {
-        short cwi = *data++ << 2;
-        u_char *cw = codebook + cwi;
-        *out++ = *cw++;
-        *out++ = *cw++;
-        *out++ = *cw++;
-        *out++ = *cw++;
-      } while (--n != -1);
-    }
-  }
-
-  MemFree(copy);
-}
-#endif
-
-#if KLANG == 1
-extern u_int AK_Progress;
-void AK_Generate(void *TmpBuf asm("a1"));
-#endif
 
 static void ShowMemStats(void) {
   Log("[Memory] CHIP: %d FAST: %d\n", MemAvail(MEMF_CHIP), MemAvail(MEMF_FAST));
@@ -179,25 +109,14 @@ static void RunEffects(void) {
   }
 }
 
+extern void InitSamples(void);
+
 int main(void) {
   /* NOP that triggers fs-uae debugger to stop and inform GDB that it should
    * fetch segments locations to relocate symbol information read from file. */
   asm volatile("exg %d7,%d7");
 
-#if VQ == 1 || DELTA == 1
-  Log("[Init] Decoding samples\n");
-  DecodeSamples(Samples, (int)SamplesSize);
-#endif
-
-#if KLANG == 1
-  Log("[Init] Generating samples\n");
-  {
-    void *TmpBuf = MemAlloc(32768, MEMF_PUBLIC);
-    AK_Generate(TmpBuf);
-    MemFree(TmpBuf);
-  }
-#endif
-
+  InitSamples();
   PtInstallCIA();
   PtInit(Module, Samples, 0);
 
