79e8a92b7459cc5e55c2f6cfec0c9751c2e9dd4b
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-03-25 23:34:37 +0100

    Shrink sinus table representation.

--- lib/libmisc/sintab.py
+++ lib/libmisc/sintab.py
@@ -7,26 +7,33 @@ if __name__ == '__main__':
     with open('sintab.c', 'w') as fh:
         with redirect_stdout(fh):
             prec = 4096
+            n = prec // 4
 
             print('#include <linkerset.h>')
             print('#include <debug.h>\n')
-            print('short sintab[%d] = {' % prec)
+            print('short sintab[%d];\n' % (prec + 1))
 
-            sintab = [int(sin(i * 2 * pi / prec) * prec) for i in range(prec)]
+            sintab = [int(sin(i * 2 * pi / prec) * prec) for i in range(n + 1)]
+
+            print('static char _sintab_encoded[%d] = {' % len(sintab))
             prev = 0
-
-            for a in sintab:
-                print('\t%d,' % (a - prev))
-                prev = a
-
+            for curr in sintab:
+                print('\t%d,' % (curr - prev))
+                prev = curr
             print('};')
+
             print('\nvoid InitSinTab(void) {')
-            print('  short sum = 0, n = %d;' % prec)
-            print('  short *tab = sintab;')
+            print('  short sum = 0, n = %d;' % n)
+            print('  char *encoded = _sintab_encoded;')
+            print('  short *inc = &sintab[0];')
+            print('  short *dec = &sintab[%d + 1];' % (n * 2))
             print('  Log("[Init] Preparing sinus table\\n");')
-            print('  while (--n >= 0) {')
-            print('    sum += *tab;')
-            print('    *tab++ = sum;')
-            print('  }')
+            print('  do {')
+            print('    sum += *encoded++;')
+            print('    inc[2048] = -sum;')
+            print('    *inc++ = sum;')
+            print('    *--dec = sum;')
+            print('    dec[2048] = -sum;')
+            print('  } while (--n != -1);')
             print('}')
             print('\nADD2INIT(InitSinTab, 0);')
