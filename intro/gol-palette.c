de6e503cb1b20429fe2a21ba0bfc3c4f632aca67
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-04-05 20:11:48 +0200

    Fix formatting using clang-format.

--- intro/gol-palette.c
+++ intro/gol-palette.c
@@ -5,27 +5,13 @@ extern TrackT GOLLogoType;
 
 // taken from the wireworld palette
 static short palette_vitruvian[16] = {
-   0x000,
-   0x3bf,
-   0x9ee,
-   0x9ee,
-   0xdff,
-   0xdff,
-   0xdff,
-   0xdff,
-   0xfff,
-   0xfff,
-   0xfff,
-   0xfff,
-   0xfff,
-   0xfff,
-   0xfff,
-   0xfff,
+  0x000, 0x3bf, 0x9ee, 0x9ee, 0xdff, 0xdff, 0xdff, 0xdff,
+  0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff, 0xfff,
 };
 
 static short palette_gol[16] = {0};
 
-static short* LoadCompressedPal(short *from, short *to) {
+static short *LoadCompressedPal(short *from, short *to) {
   *to++ = *from++;
   *to++ = *from;
   *to++ = *from++;
@@ -37,19 +23,19 @@ static short* LoadCompressedPal(short *from, short *to) {
 }
 
 static void TransitionPal(short *target_pal) {
-    u_short val, start, steps;
-    static u_short *pal;
-    static short phase = 0;
+  u_short val, start, steps;
+  static u_short *pal;
+  static short phase = 0;
 
-    val = TrackValueGet(&GOLGradientTrans, frameCount);
-    start = (val & 0xff) * gol_transitions_width;
-    steps = (val & 0xff00) >> 8;
-    if (steps > 0) {
-      phase = steps;
-      pal = gol_transitions_pixels + start;
-    }
-    if (phase) {
-      pal = LoadCompressedPal(pal, target_pal);
-      phase--;
-    }
+  val = TrackValueGet(&GOLGradientTrans, frameCount);
+  start = (val & 0xff) * gol_transitions_width;
+  steps = (val & 0xff00) >> 8;
+  if (steps > 0) {
+    phase = steps;
+    pal = gol_transitions_pixels + start;
+  }
+  if (phase) {
+    pal = LoadCompressedPal(pal, target_pal);
+    phase--;
+  }
 }
--- intro/gol-transparency.c
+++ intro/gol-transparency.c
@@ -3,27 +3,27 @@ extern TrackT GOLLogoColor;
 extern TrackT GOLLogoFade;
 
 static void ColorFadingStep(short *pal) {
-    short i;
-    short s = TrackValueGet(&GOLLogoFade, frameCount);
-    short cell_col = 0;
-    short logo_col = 0;
+  short i;
+  short s = TrackValueGet(&GOLLogoFade, frameCount);
+  short cell_col = 0;
+  short logo_col = 0;
 
-    switch(TrackValueGet(&GOLCellColor, frameCount)) {
-        case 0: cell_col = 0; break;
-        case 1: cell_col = s; break;
-        case 2: cell_col = 15-s; break;
-        case 3: cell_col = 15; break;
-    }
+  switch (TrackValueGet(&GOLCellColor, frameCount)) {
+    case 0: cell_col = 0; break;
+    case 1: cell_col = s; break;
+    case 2: cell_col = 15 - s; break;
+    case 3: cell_col = 15; break;
+  }
 
-    switch(TrackValueGet(&GOLLogoColor, frameCount)) {
-        case 0: logo_col = 0; break;
-        case 1: logo_col = s; break;
-        case 2: logo_col = 15-s; break;
-        case 3: logo_col = 15; break;
-    }
+  switch (TrackValueGet(&GOLLogoColor, frameCount)) {
+    case 0: logo_col = 0; break;
+    case 1: logo_col = s; break;
+    case 2: logo_col = 15 - s; break;
+    case 3: logo_col = 15; break;
+  }
 
-    for (i = 0; i < 8; i++) {
-      CopInsSet16(palptr + i, ColorTransition(pal[i], 0x000, cell_col));
-      CopInsSet16(palptr + i + 8, ColorTransition(pal[i], 0xfff, logo_col));
-    }
+  for (i = 0; i < 8; i++) {
+    CopInsSet16(palptr + i, ColorTransition(pal[i], 0x000, cell_col));
+    CopInsSet16(palptr + i + 8, ColorTransition(pal[i], 0xfff, logo_col));
+  }
 }
