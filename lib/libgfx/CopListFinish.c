7fa71ac6e1aa7ebd6547e630dd14376a72916883
Krystian Bacławski <krystian.baclawski@gmail.com>
2023-07-22 19:14:51 +0200

    Warn user and crash when used too much copper list entries.

--- lib/libgfx/CopListFinish.c
+++ lib/libgfx/CopListFinish.c
@@ -5,6 +5,11 @@ CopListT *CopListFinish(CopListT *list) {
   CopInsT *ins = list->curr;
   *((u_int *)ins)++ = 0xfffffffe;
   list->curr = ins;
-  Log("Used copper list slots: %ld\n", (ptrdiff_t)(list->curr - list->entry));
+  {
+    ptrdiff_t used = (ptrdiff_t)(list->curr - list->entry);
+    Log("Used copper list %p slots: %ld/%d\n", list, used, list->length);
+    if (used > list->length)
+      PANIC();
+  }
   return list;
 }
