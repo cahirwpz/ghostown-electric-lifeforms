3586c39fc75c4a3e5b00ac615954b6e9e34bdfb1
Spookbench <aleksandra.banasiak@protonmail.com>
2023-11-11 15:31:33 +0100

    Handle passing multiple instances of the same flag

--- tools/png2c/main.go
+++ tools/png2c/main.go
@@ -16,18 +16,29 @@ import (
 	"ghostown.pl/png2c/util"
 )
 
+type arrayFlag []string
+
+func (i *arrayFlag) String() string {
+	return "my string representation"
+}
+
+func (i *arrayFlag) Set(value string) error {
+	*i = append(*i, value)
+	return nil
+}
+
 var (
-	bitmapVar  string
-	pixmapVar  string
-	spriteVar  string
-	paletteVar string
+	bitmapVar  arrayFlag
+	pixmapVar  arrayFlag
+	spriteVar  arrayFlag
+	paletteVar arrayFlag
 )
 
 func init() {
-	flag.StringVar(&bitmapVar, "bitmap", "", "Output Amiga bitmap [name,dimensions,flags]")
-	flag.StringVar(&pixmapVar, "pixmap", "", "Output pixel map [name,width,height,type,flags]")
-	flag.StringVar(&spriteVar, "sprite", "", "Output Amiga sprite [name]")
-	flag.StringVar(&paletteVar, "palette", "", "Output Amiga palette [name,colors]")
+	flag.Var(&bitmapVar, "bitmap", "Output Amiga bitmap [name,dimensions,flags]")
+	flag.Var(&pixmapVar, "pixmap", "Output pixel map [name,width,height,type,flags]")
+	flag.Var(&spriteVar, "sprite", "Output Amiga sprite [name]")
+	flag.Var(&paletteVar, "palette", "Output Amiga palette [name,colors]")
 
 	flag.Parse()
 }
@@ -45,64 +56,73 @@ func main() {
 	}
 
 	var out string
-	if bitmapVar != "" {
+	if len(bitmapVar) > 0 {
 		// Check if image has a palette
 		pm, _ := img.(*image.Paletted)
 		if pm == nil {
 			panic("only paletted images are supported")
 		}
 
-		opts := pms.ParseOpts(bitmapVar,
-			pms.Param{Name: "name", CastType: pms.TYPE_STRING},
-			pms.Param{Name: "width,height,depth", CastType: pms.TYPE_INT},
-			pms.Param{Name: "extract_at", CastType: pms.TYPE_INT, Value: "-1x-1"},
-			pms.Param{Name: "interleaved", CastType: pms.TYPE_BOOL, Value: false},
-			pms.Param{Name: "cpuonly", CastType: pms.TYPE_BOOL, Value: false},
-			pms.Param{Name: "shared", CastType: pms.TYPE_BOOL, Value: false},
-			pms.Param{Name: "limit_depth", CastType: pms.TYPE_BOOL, Value: false},
-			pms.Param{Name: "onlydata", CastType: pms.TYPE_BOOL, Value: false},
-		)
+		for _, flag := range bitmapVar {
+			opts := pms.ParseOpts(flag,
+				pms.Param{Name: "name", CastType: pms.TYPE_STRING},
+				pms.Param{Name: "width,height,depth", CastType: pms.TYPE_INT},
+				pms.Param{Name: "extract_at", CastType: pms.TYPE_INT, Value: "-1x-1"},
+				pms.Param{Name: "interleaved", CastType: pms.TYPE_BOOL, Value: false},
+				pms.Param{Name: "cpuonly", CastType: pms.TYPE_BOOL, Value: false},
+				pms.Param{Name: "shared", CastType: pms.TYPE_BOOL, Value: false},
+				pms.Param{Name: "limit_depth", CastType: pms.TYPE_BOOL, Value: false},
+				pms.Param{Name: "onlydata", CastType: pms.TYPE_BOOL, Value: false},
+			)
 
-		out += b.Make(pm, cfg, opts)
+			out += b.Make(pm, cfg, opts)
+		}
+
 	}
 
-	if paletteVar != "" {
+	if len(paletteVar) > 0 {
 		// Check if image has a palette
 		pm, _ := img.(*image.Paletted)
 		if pm == nil {
 			panic("only paletted images are supported")
 		}
-		opts := pms.ParseOpts(paletteVar,
-			pms.Param{Name: p.OPT_NAME, CastType: pms.TYPE_STRING},
-			pms.Param{Name: p.OPT_COUNT, CastType: pms.TYPE_INT},
-		)
+		for _, flag := range paletteVar {
+			opts := pms.ParseOpts(flag,
+				pms.Param{Name: p.OPT_NAME, CastType: pms.TYPE_STRING},
+				pms.Param{Name: p.OPT_COUNT, CastType: pms.TYPE_INT},
+			)
 
-		out += p.Make(pm, cfg, opts)
+			out += p.Make(pm, cfg, opts)
+		}
 	}
 
-	if pixmapVar != "" {
-		opts := pms.ParseOpts(pixmapVar,
-			pms.Param{Name: "name", CastType: pms.TYPE_STRING},
-			pms.Param{Name: "width,height,bpp", CastType: pms.TYPE_INT},
-			pms.Param{Name: "limit_bpp", CastType: pms.TYPE_BOOL, Value: false},
-			pms.Param{Name: "displayable", CastType: pms.TYPE_BOOL, Value: false},
-			pms.Param{Name: "onlydata", CastType: pms.TYPE_BOOL, Value: false},
-		)
-		out += pixmap.Make(img, cfg, opts)
+	if len(pixmapVar) > 0 {
+		for _, flag := range pixmapVar {
+			opts := pms.ParseOpts(flag,
+				pms.Param{Name: "name", CastType: pms.TYPE_STRING},
+				pms.Param{Name: "width,height,bpp", CastType: pms.TYPE_INT},
+				pms.Param{Name: "limit_bpp", CastType: pms.TYPE_BOOL, Value: false},
+				pms.Param{Name: "displayable", CastType: pms.TYPE_BOOL, Value: false},
+				pms.Param{Name: "onlydata", CastType: pms.TYPE_BOOL, Value: false},
+			)
+			out += pixmap.Make(img, cfg, opts)
+		}
 	}
 
-	if spriteVar != "" {
+	if len(spriteVar) > 0 {
 		pm, _ := img.(*image.Paletted)
 		if pm == nil {
 			panic("only paletted images are supported")
 		}
-		opts := pms.ParseOpts(spriteVar,
-			pms.Param{Name: "name", CastType: pms.TYPE_STRING},
-			pms.Param{Name: "height", CastType: pms.TYPE_INT},
-			pms.Param{Name: "count", CastType: pms.TYPE_INT},
-			pms.Param{Name: "attached", CastType: pms.TYPE_BOOL, Value: false},
-		)
-		out += sprite.Make(pm, cfg, opts)
+		for _, flag := range spriteVar {
+			opts := pms.ParseOpts(flag,
+				pms.Param{Name: "name", CastType: pms.TYPE_STRING},
+				pms.Param{Name: "height", CastType: pms.TYPE_INT},
+				pms.Param{Name: "count", CastType: pms.TYPE_INT},
+				pms.Param{Name: "attached", CastType: pms.TYPE_BOOL, Value: false},
+			)
+			out += sprite.Make(pm, cfg, opts)
+		}
 	}
 
 	inName := strings.Split(r.Name(), ".")[0]
